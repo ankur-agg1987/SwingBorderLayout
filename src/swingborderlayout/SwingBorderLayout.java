ec2f3cb47d5ba87a6f30de1cbd765e3029616a6f
Ankur Kumar Aggarwal <40783014+ankur-agg1987@users.noreply.github.com>
2021-08-03 14:23:47 +0530

    multiple component in on direction commit

--- src/swingborderlayout/SwingBorderLayout.java
+++ src/swingborderlayout/SwingBorderLayout.java
@@ -1,6 +1,7 @@
 package swingborderlayout;
 
 import java.awt.BorderLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -14,6 +15,8 @@ public class SwingBorderLayout {
         JLabel l3 = new JLabel("Element3");
         JLabel l4 = new JLabel("Element4");
         JLabel l5 = new JLabel("Element5");
+        JButton bt1 = new JButton("Hello");
+        JButton bt2 = new JButton("Hello");
         
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setLayout(new BorderLayout(20, 20));
@@ -23,6 +26,9 @@ public class SwingBorderLayout {
         f.add(l3,BorderLayout.WEST);
         f.add(l4,BorderLayout.EAST);
         f.add(l5,BorderLayout.CENTER);
+        // last element at any position will replace the earlier component in that origin or direction
+        f.add(bt1,BorderLayout.NORTH);
+        f.add(bt2,BorderLayout.SOUTH);
         
         f.setSize(500,500);
         
